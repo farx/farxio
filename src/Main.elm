3fd2e42882073ae10f1f39b2bec7e8e2a9a2b9bf
Max Gnipping <max@farx.io>
2020-11-29 14:01:57 +0100

    Started from create-elm-app template

---
+++ src/Main.elm
@@ -0,0 +1,58 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, text, div, h1, img, button)
+import Html.Attributes exposing (src)
+import Svg exposing (Svg)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , h1 [] [ text "Your Elm App is working!" ]
+        , button [  ] [ text "hello" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
